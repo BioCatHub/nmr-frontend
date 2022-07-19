966ffd2686490d5449741b851dde541e27c6ac63
Stephan Malzacher <s.malzacher@web.de>
2022-07-19 23:40:39 +0200

    feat: plotly added :rocket:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as PlotlyJS from 'plotly.js/dist/plotly.js';
+import { PlotlyModule } from 'angular-plotly.js';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { VisualisationComponent } from './visualisation/visualisation.component'
 //import { CdsModule } from '@cds/angular';
 //import { ClarityIcons, userIcon } from '@cds/core/icon';
+
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    VisualisationComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +29,9 @@ import { HttpClientModule } from '@angular/common/http'
     //CdsModule,
     //ClarityIcons,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule,
+    PlotlyModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/visualisation/visualisation.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visualisation',
+  templateUrl: './visualisation.component.html',
+  styleUrls: ['./visualisation.component.css']
+})
+export class VisualisationComponent implements OnInit {
+
+  graph2 = {
+    data: [
+      { x: [1, 2, 3, 4, 5], y: [1, 4, 9, 4, 1], type: 'scatter' },
+      { x: [1, 2, 3, 4, 5], y: [1, 3, 6, 9, 6], type: 'scatter' },
+      { x: [1, 2, 3, 4, 5], y: [1, 2, 4, 5, 6], type: 'scatter' },
+    ],
+    layout: {title: 'Reaction flow'}
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
